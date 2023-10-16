c21bbdc5e84abdb39a711f44abb19ad900dd285d
alexyspol <alexsandroyspol@gmail.com>
2023-10-16 00:55:46 -0300

    Added deploy.yml for GitHub Actions
    Added a deploy.yml file to configure GitHub Actions for automated deployment of the Astro project. This will ensure that changes are automatically built and deployed when pushed to the repository.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue()],
+  site: 'https://alexyspol.github.io',
+  base: '/frontendmentor-todo-app'
 });
